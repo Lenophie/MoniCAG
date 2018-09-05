43ffefdb3a247dd7e6b5bbd14846ac10153583aa
Lenophie <quasiphies@gmail.com>
2018-09-05 22:37:47 +0200

    Fix mobile viewport

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="author" content="Lenophie" />
         <link href="@yield('stylesheet')" rel="stylesheet" />
