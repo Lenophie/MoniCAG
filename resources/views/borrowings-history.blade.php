0f4501219518945ff95f05219f7120088fab4740
Lenophie <quasiphies@gmail.com>
2018-09-28 12:04:23 +0200

    Add "Borrowings history" view handling of deleted users

--- resources/views/borrowings-history.blade.php
+++ resources/views/borrowings-history.blade.php
@@ -57,13 +57,34 @@
                         @foreach($borrowings as $borrowing)
                             <tr>
                                 <td>{{$borrowing->startDate->format('d/m/Y')}}</td>
-                                <td>{{$borrowing->borrower->firstName}} {{strtoupper($borrowing->borrower->lastName)}}</td>
+                                <td>
+                                    @if($borrowing->borrower)
+                                        {{$borrowing->borrower->firstName}} {{strtoupper($borrowing->borrower->lastName)}}
+                                    @else
+                                        <span class="deleted-user-span">Utilisateur supprimé</span>
+                                    @endif
+                                </td>
                                 <td>{{$borrowing->inventoryItem->name}}</td>
                                 <td>{{number_format($borrowing->guarantee, 2)}} €</td>
-                                <td class="border-right">{{$borrowing->initialLender->firstName}} {{strtoupper($borrowing->initialLender->lastName)}}</td>
+                                <td class="border-right">
+                                    @if($borrowing->initialLender)
+                                        {{$borrowing->initialLender->firstName}} {{strtoupper($borrowing->initialLender->lastName)}}
+                                    @else
+                                        <span class="deleted-user-span">Utilisateur supprimé</span>
+                                    @endif
+                                </td>
                                 <td>{{$borrowing->expectedReturnDate->format('d/m/Y')}}</td>
-                                <td>@if($borrowing->returnDate){{$borrowing->returnDate->format('d/m/Y')}}@endif</td>
-                                <td>@if($borrowing->returnLender){{$borrowing->returnLender->firstName}} {{strtoupper($borrowing->returnLender->lastName)}}@endif</td>
+                                <td>
+                                    @if($borrowing->returnDate)
+                                        {{$borrowing->returnDate->format('d/m/Y')}}
+                                    @endif
+                                </td>
+                                <td>
+                                    @if($borrowing->returnLender)
+                                        {{$borrowing->returnLender->firstName}} {{strtoupper($borrowing->returnLender->lastName)}}
+                                    @elseif($borrowing->returnDate !== null)
+                                        <span class="deleted-user-span">Utilisateur supprimé</span>
+                                    @endif</td>
                             </tr>
                         @endforeach
                     </tbody>
