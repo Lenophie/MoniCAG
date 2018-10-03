f985700acaa09258751d0c57724be3fec93deea9
Lenophie <quasiphies@gmail.com>
2018-10-03 10:30:05 +0200

    Generate unique password in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'last_name' => $faker->lastName,
         'promotion' => rand($year - 7, $year + 7),
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt($faker->unique()->password),
         'role_id' => UserRole::NONE,
         'remember_token' => str_random(10),
     ];
