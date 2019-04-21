abfa66925967d6f2575fce24732693c3485b0c32
Lenophie <quasiphies@gmail.com>
2019-04-22 01:15:24 +0200

    Fix user edition error messages

--- resources/lang/fr/validation/deleteUser.php
+++ resources/lang/fr/validation/deleteUser.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'userId.required' => 'Veuillez sélectionner un utilisateur à éditer.',
-    'userId.integer' => 'Veuillez sélectionner, par son id, un utilisateur à éditer.',
-    'userId.exists' => 'Cet utilisateur n\'existe pas dans notre base de données.',
-    'userId.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de modifier un autre administrateur.',
-    'userId.not_involved_in_a_current_borrowing' => 'Un utilisateur impliqué dans un emprunt en cours ne peut pas être supprimé.'
+    'user.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de modifier un autre administrateur.',
+    'user.not_involved_in_a_current_borrowing' => 'Un utilisateur impliqué dans un emprunt en cours ne peut pas être supprimé.'
 ];
--- resources/lang/fr/validation/patchUser.php
+++ resources/lang/fr/validation/patchUser.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-    'userId.required' => 'Veuillez sélectionner un utilisateur à modifier.',
-    'userId.integer' => 'Veuillez sélectionner, par son id, un utilisateur à modifier.',
-    'userId.exists' => 'Cet utilisateur n\'existe pas dans notre base de données.',
-    'userId.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de modifier un autre administrateur.',
+    'user.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de modifier un autre administrateur.',
     'role.required' => 'Veuillez sélectionner un rôle.',
     'role.integer' => 'Veuillez sélectionner un rôle valide par son id.',
     'role.exists' => 'Ce rôle n\'existe pas dans notre base de données.'
