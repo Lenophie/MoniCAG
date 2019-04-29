c6065228b3956ed2e323a9d77f3674f0230b50af
Lenophie <quasiphies@gmail.com>
2019-04-29 22:27:52 +0200

    Try fixing mobile viewport

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{App::getLocale()}}">
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3, minimum-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="author" content="Lenophie" />
         <meta name="CSRF-TOKEN" content="{{ csrf_token() }}">
