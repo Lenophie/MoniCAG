bccdc794afd6cdf7c675587f1156e840e2ec7ebb
Lenophie <quasiphies@gmail.com>
2019-04-30 03:15:31 +0200

    Add account deletion modal body Vue component

---
+++ resources/js/components/modalBodies/accountDeletionModalBody.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="delete-modal-body">
+        <p><span v-html="message"></span></p>
+        <hr>
+        <form id="delete-form" autocomplete="off">
+            <div class="field">
+                <label class="label" for="password">{{confirmPasswordLabel}}</label>
+                <div class="control has-icons-left">
+                    <input class="input" type="password" id="password" name="password" required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-key"></i>
+                    </span>
+                </div>
+            </div>
+            <div id="errors-field-password"></div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['message', 'confirmPasswordLabel']
+    }
+</script>
--- resources/js/vue.js
+++ resources/js/vue.js
@@ -1,5 +1,6 @@
 window.Vue = require('vue');
 Vue.component('modal', require('./components/modal.vue').default);
+Vue.component('account-deletion-modal-body', require('./components/modalBodies/accountDeletionModalBody.vue').default);
 
 new Vue({
     el: '#app'
