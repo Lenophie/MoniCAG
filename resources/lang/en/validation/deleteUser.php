483f52e11be579ee8f4ef545ede58b0df3487f93
Lenophie <quasiphies@gmail.com>
2019-08-08 20:15:25 +0200

    Improve some translation strings

--- resources/lang/en/validation/deleteUser.php
+++ resources/lang/en/validation/deleteUser.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'user.unchanged_if_other_admin' => 'You don\'t have the permission to modify another administrator.',
+    'user.unchanged_if_other_admin' => 'You don\'t have the permission to delete another administrator.',
     'user.not_involved_in_a_current_borrowing' => 'A user involved in an ongoing borrowing can\'t be deleted.'
 ];
--- resources/lang/fr/validation/deleteUser.php
+++ resources/lang/fr/validation/deleteUser.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'user.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de modifier un autre administrateur.',
+    'user.unchanged_if_other_admin' => 'Vous n\'avez pas la permission de supprimer un autre administrateur.',
     'user.not_involved_in_a_current_borrowing' => 'Un utilisateur impliqué dans un emprunt en cours ne peut pas être supprimé.'
 ];
