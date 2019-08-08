2f9612ba3740386944db7e02f982872dcb768c01
Lenophie <quasiphies@gmail.com>
2019-08-08 04:00:42 +0200

    Add checkout button Vue component

---
+++ resources/js/components/newBorrowing/checkoutButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <a class="fa-layers fa-fw" id="checkout-link" @click="$emit('open')">
+        <i class="fas fa-gift"></i>
+        <span class="fa-layers-counter" id="checkout-counter">{{counter}}</span>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            counter: Number
+        },
+    }
+</script>
