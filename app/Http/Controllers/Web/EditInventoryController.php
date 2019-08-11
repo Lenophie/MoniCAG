4d0c4ac27a8f83f7b36d59f16f84996ed2a157ec
Lenophie <quasiphies@gmail.com>
2019-08-11 23:47:50 +0200

    Use resources in EditInventoryController

--- app/Http/Controllers/Web/EditInventoryController.php
+++ app/Http/Controllers/Web/EditInventoryController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Web;
 
 use App\Genre;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\GenreResource;
+use App\Http\Resources\API\InventoryItemStatusResource;
+use App\Http\Resources\Views\DetailedInventoryItemResource;
 use App\InventoryItem;
 use App\InventoryItemStatus;
 use Illuminate\Http\Response;
@@ -19,9 +22,10 @@ class EditInventoryController extends Controller
     {
         abort_unless(Gate::allows('update', InventoryItem::class), Response::HTTP_FORBIDDEN);
 
-        $inventoryItems = InventoryItem::allNotTranslatedJoined();
-        $genres = Genre::allTranslated();
-        $inventoryStatuses = InventoryItemStatus::allTranslated();
+        $inventoryItems = DetailedInventoryItemResource::collection(InventoryItem::all());
+        $genres = GenreResource::collection(Genre::translated()->get());
+        $inventoryStatuses = InventoryItemStatusResource::collection(InventoryItemStatus::translated()->get());
+
         return view('edit-inventory', compact('inventoryItems', 'genres', 'inventoryStatuses'));
     }
 }
--- app/InventoryItemStatus.php
+++ app/InventoryItemStatus.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 
 class InventoryItemStatus extends Model
@@ -17,17 +16,9 @@ class InventoryItemStatus extends Model
     public const UNKNOWN = 5;
 
     /**
-     * @return Collection
-     */
-    public static function allTranslated() {
-        $statuses = InventoryItemStatus::selectLocale()->get();
-        return $statuses;
-    }
-
-    /**
      * @return Builder
      */
-    private static function selectLocale() {
+    public static function translated() {
         return InventoryItemStatus::select('id', 'name_'.App::getLocale().' AS name');
     }
 }
