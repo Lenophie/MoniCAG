1f4fdbf8dc24f63f23a2ff024a5c5af1cb6fa2cd
Lenophie <quasiphies@gmail.com>
2019-08-14 01:42:34 +0200

    Refactor Borrowing model

--- app/Borrowing.php
+++ app/Borrowing.php
@@ -3,18 +3,11 @@
 namespace App;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\App;
 
 class Borrowing extends Model
 {
-    /**
-     * Indicates whether attributes are snake cased on arrays.
-     *
-     * @var bool
-     */
-    public static $snakeAttributes = false;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -48,24 +41,18 @@ class Borrowing extends Model
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The accessors to append to the model's array form.
      *
      * @var array
      */
-    protected $casts = [
-        'startDate' => 'date:d/m/Y',
-        'expectedReturnDate' => 'date:d/m/Y',
-        'returnDate' => 'date:d/m/Y'
-    ];
+    protected $appends = ['is_late'];
 
     /**
      * Get the initial lender associated with the borrowing.
      */
     public function initialLender()
     {
-        return $this
-            ->hasOne('App\User', 'id', 'initial_lender_id')
-            ->select('id', 'first_name AS firstName', 'last_name as lastName', 'promotion');
+        return $this->hasOne('App\User', 'id', 'initial_lender_id');
     }
 
     /**
@@ -73,9 +60,7 @@ class Borrowing extends Model
      */
     public function returnLender()
     {
-        return $this
-            ->hasOne('App\User', 'id', 'return_lender_id')
-            ->select('id', 'first_name AS firstName', 'last_name as lastName', 'promotion');
+        return $this->hasOne('App\User', 'id', 'return_lender_id');
     }
 
 
@@ -84,9 +69,7 @@ class Borrowing extends Model
      */
     public function borrower()
     {
-        return $this
-            ->hasOne('App\User', 'id', 'borrower_id')
-            ->select('id', 'first_name AS firstName', 'last_name as lastName', 'promotion');
+        return $this->hasOne('App\User', 'id', 'borrower_id');
     }
 
     /**
@@ -94,92 +77,34 @@ class Borrowing extends Model
      */
     public function inventoryItem()
     {
-        return $this
-            ->hasOne('App\InventoryItem', 'id', 'inventory_item_id')
-            ->select('id', 'name_'.App::getLocale().' AS name');
+        return $this->hasOne('App\InventoryItem', 'id', 'inventory_item_id');
     }
 
-    public static function current() {
-        $borrowings = Borrowing::initialJoin()
-            ->where('finished', false)
-            ->orderByDate()
-            ->get();
+    /**
+     * Get the borrowing's lateness status.
+     *
+     * @return string
+     */
+    public function getIsLateAttribute() {
+        $expectedReturnDate = Carbon::createFromFormat('Y-m-d', $this->attributes["expected_return_date"]);
 
-        foreach($borrowings as $borrowing) {
-            self::removeIds($borrowing);
-            self::checkLateness($borrowing);
+        if ($this->attributes["return_date"] !== null) {
+            $returnDate = Carbon::createFromFormat('Y-m-d', $this->attributes["return_date"]);
+            return $returnDate->gt($expectedReturnDate);
         }
 
-        return $borrowings;
+        $currentDate = Carbon::now()->startOfDay();
+        return $currentDate->gt($expectedReturnDate);
     }
 
-    public static function history() {
-        $borrowings = Borrowing::completeJoin()
-            ->orderByDate()
-            ->get();
-
-        foreach($borrowings as $borrowing) {
-            self::removeIds($borrowing);
-        }
-
-        return $borrowings;
-    }
-
-    public static function userCurrentHistory($userId) {
-        $borrowings = Borrowing::completeJoin()
-            ->where([
-                'borrower_id' => $userId,
-                'finished' => 0])
-            ->orderByDate()
-            ->get();
-
-        foreach($borrowings as $borrowing) {
-            self::removeIds($borrowing);
-        }
-
-        return $borrowings;
-    }
-
-    private static function initialJoin() {
-        return Borrowing::with(['initialLender', 'borrower', 'inventoryItem'])
-            ->select('id',
-                'inventory_item_id',
-                'finished',
-                'initial_lender_id',
-                'borrower_id',
-                'start_date AS startDate',
-                'expected_return_date AS expectedReturnDate',
-                'guarantee');
-    }
-
-    private static function completeJoin() {
-        return Borrowing::with(['initialLender', 'returnLender', 'borrower', 'inventoryItem'])
-            ->select('id',
-                'inventory_item_id',
-                'finished',
-                'initial_lender_id',
-                'return_lender_id',
-                'borrower_id',
-                'start_date AS startDate',
-                'expected_return_date AS expectedReturnDate',
-                'return_date AS returnDate',
-                'guarantee');
-    }
-
-    private static function removeIds($borrowing) {
-        unset($borrowing->inventory_item_id);
-        unset($borrowing->initial_lender_id);
-        unset($borrowing->return_lender_id);
-        unset($borrowing->borrower_id);
-    }
-
-    private static function checkLateness($borrowing) {
-        if (Carbon::now()->startOfDay()->gt($borrowing->expectedReturnDate)) $borrowing->isLate = true;
-        else $borrowing->isLate = false;
-    }
-
+    /**
+     * Scope a query to order by dates.
+     *
+     * @param  Builder  $query
+     * @return Builder
+     */
     public static function scopeOrderByDate($query) {
-        return $query->orderBy('expectedReturnDate', 'desc')
-            ->orderBy('startDate', 'desc');
+        return $query->orderBy('expected_return_date', 'desc')
+            ->orderBy('start_date', 'desc');
     }
 }
