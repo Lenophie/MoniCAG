c035fbb45bad8ef61b335988592b22e2fd3dfc99
Lenophie <quasiphies@gmail.com>
2019-08-26 21:53:37 +0200

    Add Number Input component

---
+++ resources/js/components/numberInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input
+        ref="input"
+        class="input"
+        :class="{'is-danger': isInvalid}"
+        type="number"
+        :value="value"
+        @input="handleInput"
+    >
+</template>
+
+<script>
+    export default {
+        name: "numberInput",
+        props: {
+            value: String
+        },
+        data: function () {
+            return {
+                isInvalid: false,
+            }
+        },
+        methods: {
+            handleInput: function ($event) {
+                this.isInvalid = !this.$refs.input.checkValidity();
+                this.$emit('input', $event.target.value);
+            }
+        }
+    }
+</script>
