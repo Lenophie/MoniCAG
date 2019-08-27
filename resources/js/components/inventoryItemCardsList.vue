e40642d903ba5a86704d38d4de8cda269c93038a
Lenophie <quasiphies@gmail.com>
2019-08-27 19:59:45 +0200

    Add InventoryItemCardsList component

---
+++ resources/js/components/inventoryItemCardsList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="columns is-multiline">
+        <div class="column is-2" v-for="inventoryItem in inventoryItems">
+            <inventory-item-card :inventory-item="inventoryItem"></inventory-item-card>
+        </div>
+    </div>
+</template>
+
+<script>
+    import inventoryItemCard from "./inventoryItemCard.vue";
+
+    export default {
+        name: "inventoryItemCardsList",
+        props: {
+            inventoryItems: Array
+        },
+        components: { inventoryItemCard },
+        mounted() {
+            this.$nextTick(function () {
+                this.$emit('mounted');
+            });
+        }
+    }
+</script>
