c936a95697cf6a0499d1f145fe2b3a53f518ea3d
Lenophie <quasiphies@gmail.com>
2019-08-28 16:51:12 +0200

    Add missing translation string for Edit Inventory page

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -62,9 +62,11 @@ return [
     'edit_inventory' => [
         'change_status' => 'Change status',
         'add_genre' => 'Add genre',
+        'edit_genres' => 'Edit genres',
+        'edit_genre' => 'Edit genre',
+        'add_item' => 'Add item',
         'edit_items' => 'Edit items',
         'edit_item' => 'Edit item',
-        'add_item' => 'Add item',
         'deletion_title' => 'Confirm deletion',
         'deletion_warning' => "Deleting an item should only be done if the item doesn't, and never did, correspond to an actual inventory item.</br>If the item was lost, you should rather change its status.</br>After deleting an item, all its borrowings will be deleted as well from the database.</br><b>This deletion can't be undone.</b>"
     ],
