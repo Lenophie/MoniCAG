cbe92ab2c02fb3367b7e447bb6c3bf798bc5ea8d
Lenophie <quasiphies@gmail.com>
2019-09-06 00:32:50 +0200

    Rename user role patching route

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ Route::apiResource('borrowings', 'BorrowingController')->except(['update', 'dele
 Route::patch('borrowings', 'BorrowingController@return')->name('borrowings.return');
 Route::apiResource('inventoryItems', 'InventoryItemController');
 Route::apiResource('users', 'UserController')->except(['store', 'update']);
-Route::patch('users/{user}/role', 'UserController@updateRole')->name('user.changeRole');
+Route::patch('users/{user}/role', 'UserController@updateRole')->name('users.changeRole');
 Route::apiResource('genres', 'GenreController')->except(['show']);
