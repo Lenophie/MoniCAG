d2e8aad04acb1eb09011af712107fbe246ed3ac5
Lenophie <quasiphies@gmail.com>
2019-09-06 00:29:20 +0200

    Refactor Edit Users Controller

--- app/Http/Controllers/Web/EditUsersController.php
+++ app/Http/Controllers/Web/EditUsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\UserResource;
+use App\Http\Resources\API\UserRoleResource;
 use App\User;
 use App\UserRole;
 use Illuminate\Http\Response;
@@ -18,8 +20,19 @@ class EditUsersController extends Controller
     {
         abort_unless(Gate::allows('viewAny', User::class), Response::HTTP_FORBIDDEN);
 
-        $users = User::allSelected();
-        $userRoles = UserRole::allTranslated();
-        return view('edit-users', compact('users', 'userRoles'));
+        $users = UserResource::collection(User::all());
+        $userRoles = UserRoleResource::collection(UserRole::translated()->get());
+
+        $compactData = [
+            'resources' => [
+                'users' => $users,
+                'userRoles' => $userRoles
+            ],
+            'routes' => [
+                'users' => route('users.index')
+            ]
+        ];
+
+        return view('edit-users', compact('compactData'));
     }
 }
