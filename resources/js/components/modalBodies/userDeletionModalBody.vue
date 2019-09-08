bb3d4c01e3cab2f76e391a7487740712679a1411
Lenophie <quasiphies@gmail.com>
2019-09-08 21:52:34 +0200

    Add modal bodies Vue components for Edit Users page

---
+++ resources/js/components/modalBodies/userDeletionModalBody.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <p class="has-text-danger">
+            <span v-html="trans('messages.edit_users.user_deletion_warning')"></span>
+        </p>
+        <hr>
+        <form :action="userDeletionRequest.route" autocomplete="off" v-on:submit.prevent="submit">
+            <div class="field">
+                <label class="label" for="user-deletion-confirm-password-input">{{ trans('Confirm password') }}</label>
+                <div class="control has-icons-left">
+                    <input
+                        class="input"
+                        type="password"
+                        id="user-deletion-confirm-password-input"
+                        name="password"
+                        :placeholder="trans('Password')"
+                        v-model="userDeletionRequest.params.password"
+                        :disabled=userDeletionRequest.isProcessing
+                        required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-key"></i>
+                    </span>
+                </div>
+            </div>
+            <error-field
+                :errors-list="userDeletionRequest.errors"
+                :field-path="'password'">
+            </error-field>
+            <error-field
+                :errors-list="userDeletionRequest.errors"
+                :field-path="'user'">
+            </error-field>
+        </form>
+    </div>
+</template>
+
+<script>
+    import errorField from '../errorField.vue';
+
+    export default {
+        name: "userDeletionModalBody",
+        props: {
+            userDeletionRequest: {
+                type: Object,
+                required: true
+            },
+            submit: {
+                type: Function,
+                required: true
+            }
+        },
+        components: {
+            errorField
+        }
+    }
+</script>
---
+++ resources/js/components/modalBodies/userRoleUpdateModalBody.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <form :action="userRoleUpdateRequest.route" autocomplete="off" v-on:submit.prevent="submit">
+            <div class="field">
+                <label class="label" for="role-select">{{ trans('Role') }}</label>
+                <div class="control">
+                    <div class="select is-fullwidth">
+                        <select
+                            v-if="userRoleUpdateRequest.params.role != null"
+                            name="role"
+                            id="role-select"
+                            v-model="userRoleUpdateRequest.params.role">
+                            <option
+                                v-for="userRole in userRoles"
+                                :value="userRole"
+                                :selected="userRole === userRoleUpdateRequest.params.role">
+                                {{ userRole.name }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <error-field
+                :errors-list="userRoleUpdateRequest.errors"
+                :field-path="'role'">
+            </error-field>
+            <div class="field">
+                <label class="label" for="user-role-update-confirm-password-input">{{ trans('Confirm password') }}</label>
+                <div class="control has-icons-left">
+                    <input
+                        class="input"
+                        type="password"
+                        id="user-role-update-confirm-password-input"
+                        name="password"
+                        :placeholder="trans('Password')"
+                        v-model="userRoleUpdateRequest.params.password"
+                        :disabled=userRoleUpdateRequest.isProcessing
+                        required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-key"></i>
+                    </span>
+                </div>
+            </div>
+            <error-field
+                :errors-list="userRoleUpdateRequest.errors"
+                :field-path="'password'">
+            </error-field>
+        </form>
+    </div>
+</template>
+
+<script>
+    import errorField from '../errorField.vue';
+
+    export default {
+        name: "userRoleUpdateModalBody",
+        props: {
+            userRoleUpdateRequest: {
+                type: Object,
+                required: true
+            },
+            userRoles: {
+                type: Array,
+                required: true
+            },
+            submit: {
+                type: Function,
+                required: true
+            }
+        },
+        components: {
+            errorField
+        }
+    }
+</script>
