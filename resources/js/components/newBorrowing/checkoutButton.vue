55de67f15f91152eafdac836ab662eb9e2374b74
Lenophie <quasiphies@gmail.com>
2019-09-18 14:36:00 +0200

    Fix Checkout button component tabindex

--- resources/js/components/newBorrowing/checkoutButton.vue
+++ resources/js/components/newBorrowing/checkoutButton.vue
@@ -3,7 +3,7 @@
         class="fa-layers fa-fw"
         id="checkout-link"
         @click="$emit('open')"
-        :tabable="tabable ? 0 : -1"
+        :tabindex="tabable ? 0 : -1"
     >
         <i class="fas fa-save"></i>
         <span class="fa-layers-counter" id="checkout-counter">{{counter}}</span>
